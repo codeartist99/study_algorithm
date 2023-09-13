05ca94b3bba75985c0e5bbadc104d8e5e4df4f1e
seongwon <53917860+codeartitect@users.noreply.github.com>
2023-09-13 11:22:54 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 65.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (height < array[i]) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
