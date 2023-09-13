e4b482a4a0a1bac4363dc39e4c5890b5d5d80d30
seongwon <53917860+codeartitect@users.noreply.github.com>
2023-09-13 11:22:09 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                answer = answer + n;
+            }
+            n = n - 1;
+        }
+        return answer;
+    }
+}
