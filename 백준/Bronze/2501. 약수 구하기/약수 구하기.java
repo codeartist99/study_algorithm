824a95689cdd3873b5b4c2cfc12d4f3a901c2de4
Seongwon <53917860+codeartitect@users.noreply.github.com>
2023-10-18 14:34:25 +0900

    [Bronze III] Title: 약수 구하기, Time: 224 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {	
+		Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) result.add(i);
+        }
+        if (k <= result.size()) {
+            System.out.println(result.get(k - 1));
+        } else {
+            System.out.println(0);
+        }
+   }
+}
