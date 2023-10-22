607cbb14083443b04c80c5ea5e585d56af3863e1
codeartitect <ysw991106@gmail.com>
2023-10-22 17:51:42 +0900

    버블 정렬

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class BubbleSort {
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
